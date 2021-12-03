f87181aff17104d4d250c1b7ddae8f7b2f558686
prithviBytes <prithvisuvarna56@gmail.com>
2021-12-03 15:43:02 +0530

    add: test exec

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to this file.
+          I made some changes
         </p>
         <a
           className="App-link"
